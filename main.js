427fbe28ffbae520558dbcb09eb57ded6b17698a
IloveGreeny <94252672+IloveGreeny@users.noreply.github.com>
2022-06-04 14:57:03 +0400

    Update main.js

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ const {BOT_TOKEN} = process.env;
 const bot = new Telegraf(BOT_TOKEN);
 const say = require("say");
 
-let waits_for_text = false;
+let waits-for-text = false;
 
 bot.start(ctx => {
     ctx.reply('Programming is cool!')
 });
 
 bot.command('audio', ctx => {
-    waits_for_text = true
+    waits-for-text = true
     ctx.reply('Enter your text!')
 });
 
 bot.on('text', ctx => {
-    if (!waits_for_text) {
+    if (!waits-for-text) {
         return ctx.reply('What?.')
     }
 
